1a4a2ba311e02c8ea2d68c8af558afbd85f7816a
unknown <ramovic225@gmail.com>
2024-09-26 13:32:09 +0200

    added navbar bg color while scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 const Navbar = ({ scrollToSection }) => {
 
+    const [isTop, setIsTop] = useState(true);
+
     const navMenu = [
         {
             name: 'Ganića Kula',
@@ -13,8 +15,25 @@ const Navbar = ({ scrollToSection }) => {
         },
     ]
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 96) {
+                setIsTop(false)
+            } else {
+                setIsTop(true)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    }, [isTop, setIsTop])
+
     return (
-        <div className={`flex justify-end h-16 gap-20 sticky top-0 z-50 px-10`}>
+        <div className={`flex justify-end h-16 gap-20 sticky top-0 z-50 px-10 ${isTop ? 'bg-transparent' : 'bg-white text-black shadow-xl'}`}>
             {navMenu.map((menu, index) => (
                 <div key={index} className="hover:underline hover:underline-offset-8 cursor-pointer" onClick={() => scrollToSection(menu.ref)}>
                     <p className="text-lg md:text-xl py-4">{menu.name}</p>
--- src/sections/FooterSection.jsx
+++ src/sections/FooterSection.jsx
@@ -1,7 +1,7 @@
 const FooterSection = () => {
     return (
         <div className="flex justify-center pb-1 pt-4 text-center">
-            <p className="text-lg">Iznijeti stavovi su isključivo odgovornost autora i/ili izdavača i nužno ne izražavaju stavove Fonda.</p>
+            <p className="text-base md:text-lg">Iznijeti stavovi su isključivo odgovornost autora i/ili izdavača i nužno ne izražavaju stavove Fonda.</p>
         </div>
     )
 }
